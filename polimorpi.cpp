59b3853737d7d0a259090c865713ba8093a251a9
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-06-03 17:28:36 +0700

    Mendefinisikan kelas seseorang dengan fungsi virtual pesan(), yang bisa dioverride oleh kelas turunan.

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,6 +1,16 @@
 #include <iostream> 
 using namespace std;
 
+class seseorang{
+    public:
+    virtual void pesan(){
+        //virtual void pesan(){
+        // cout << "Pesan dari seseorang" << endl;
+    }
+};
+
+
+
 int main(){
-    
+
 }
