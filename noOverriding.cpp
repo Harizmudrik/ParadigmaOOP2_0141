9405cc009ed19367582b44e93e8fafc0ce236935
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-06-03 18:47:33 +0700

    Membuat fungsi untuk menimpa fungsi dari kelas dasar supaya saat dipanggil, fungsi kelas turunan yang dijalankan

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 using namespace std;
 
+class baseClass{
+    public:
+        virtual void perkenalan(){
+            cout << "Hallo nama saya Function dari base class";
+        }
+};
+
+class deriverClass : public baseClass{
+    public:
+        void perkenalan(){
+            cout << "Hallo saya Function dari deriver class";
+        }
+};
+
 int main(){
+    deriverClass a;
+    a.perkenalan();
+    return 0;   
 }
